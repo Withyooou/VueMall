40c908495c6482fca7772a0a55e43637dc4aef32
沈伟煜 <923207467@qq.com>
2020-11-30 16:19:44 +0800

    请求到首页商品数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,43 +3,87 @@
     <nav-bar class="home-nav"><template v-slot:center>购物街</template></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabcontrol/TabControl'
+
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
-import {getHomeMultidata} from 'network/home'
+import FeatureView from './childComps/FeatureView'
+
+import { getHomeMultidata, getHomeGoods } from 'network/home'
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    TabControl,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      console.log(res);
-      // 获取轮播图数据
-      this.banners = res.data.banner.list;
-      // 获取轮播图下面的推荐数据;
-      this.recommends = res.data.recommend.list;
-    })
+    // 1.请求轮播图、推荐小圆圈数据
+    this.getHomeMultidata()
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        // 获取轮播图数据
+        this.banners = res.data.banner.list;
+        // 获取轮播图下面的推荐数据;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        console.log(res)
+        this.goods[type].list.push(...res.data.list)  // ES6扩展运算符
+        this.goods[type].page ++
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    height: 2500px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    right: 0;
+    left: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
